788060fde6360bedefe73bc6cb23b7374c5c07d5
zawarali0302-commits <zawarali0302@gmail.com>
2026-03-18 22:38:43 +0500

    ...

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../app/generated/prisma/client";
-
+import { PrismaClient } from "../app/generated/prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient; 
 }; 
